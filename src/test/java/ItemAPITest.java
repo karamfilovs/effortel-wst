3de40f55cbcd03318781380cbc66a3caf3d88afc
Aleksandar Karamfilov <aleksandar.karamfilov.extern@joyn.de>
2022-03-09 19:47:04 +0200

    Added JUnit 5 library

---
+++ src/test/java/ItemAPITest.java
@@ -0,0 +1,15 @@
+import org.junit.jupiter.api.Assertions;
+import org.junit.jupiter.api.DisplayName;
+import org.junit.jupiter.api.Tag;
+import org.junit.jupiter.api.Test;
+
+public class ItemAPITest {
+
+    @Test
+    @DisplayName("Can create new item")
+    @Tag("api")
+    @Tag("positive")
+    void canCreateNewItem(){
+
+    }
+}
